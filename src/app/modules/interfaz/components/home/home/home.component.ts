ca54e588e57d74cb2365032a57a5e797f54f12fb
cristobal <cristobal.canas@rendme.com>
2023-12-11 09:32:38 -0300

    avance 2

--- src/app/modules/interfaz/components/home/home/home.component.ts
+++ src/app/modules/interfaz/components/home/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Page1Component } from '../../page1/page1.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+  Page1Component
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
